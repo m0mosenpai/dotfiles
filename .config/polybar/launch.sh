7af9733b9f85c889313a3eb7dd25d0130d2f4042
Sarthak Khattar <sarthakoct@gmail.com>
2024-08-31 20:55:34 -0500

    added initial dotfiles

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# For multiple monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Bars launched..."
